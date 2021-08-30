adff48b9ecfa5e3f7f659a308f932a99a346bf23
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:14:37 +0500

    modified home page

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ header.style.cssText = 'display: grid; grid-template-columns: 1fr 1fr 1fr;';
 home.style.cssText = 'border: 1px solid black; text-align: center; font-size: 35px;';
 menu.style.cssText = 'border: 1px solid black; text-align: center; font-size: 35px;';
 contact.style.cssText = 'border: 1px solid black; text-align: center; font-size: 35px;';
+
+body.appendChild(homeDiv);
+body.appendChild(matn);
+// home button
+home.style.cssText = 'color: white; border: 1px solid white; text-align: center; font-size: 35px;';
+body.style.cssText = 'background-size: 100%; background-image: url(http://images.summitmedia-digital.com/esquiremagph/images/2020/09/07/junk-food-aging.jpg); background-repeat: no-repeat';
+homeDiv.textContent = 'Home Page';
+homeDiv.style.cssText = 'color: white; text-align: center; font-family: sans-serif';
+matn.textContent = "Welcome to Fast-Food Restaurant";
+matn.style.cssText = 'color: white; font-size: 20px ';
