706ab78c4dfd7438f80258e17f1de769668e4335
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:04:36 +0500

    called the elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const header = document.querySelector('header');
+const home = document.querySelector('.home');
+const menu = document.querySelector('.menu');
+const contact = document.querySelector('.contact');
+const body = document.querySelector('body');
