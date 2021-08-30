0092b9e97e73419d7bdbfb262b6c7dfd409c95fe
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:12:26 +0500

    gave the menu a style

--- script.js
+++ script.js
@@ -6,3 +6,4 @@ const body = document.querySelector('body');
 
 header.style.cssText = 'display: grid; grid-template-columns: 1fr 1fr 1fr;';
 home.style.cssText = 'border: 1px solid black; text-align: center; font-size: 35px;';
+menu.style.cssText = 'border: 1px solid black; text-align: center; font-size: 35px;';
