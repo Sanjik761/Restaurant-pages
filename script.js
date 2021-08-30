bbd8c331127ae50d2c69d5e820869833603b8c9e
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:24:02 +0500

    modified js

--- script.js
+++ script.js
@@ -23,32 +23,31 @@ matn.textContent = "Welcome to Fast-Food Restaurant";
 matn.style.cssText = 'color: white; font-size: 20px ';
 
 home.addEventListener('click', () => {
-    body.style.cssText = 'background-size: 100%; background-image: url(http://images.summitmedia-digital.com/esquiremagph/images/2020/09/07/junk-food-aging.jpg); background-repeat: no-repeat';
-    homeDiv.textContent = 'Home Page';
-    homeDiv.style.cssText = 'color: white; text-align: center; font-family: sans-serif';
-    matn.textContent = "Welcome to Fast-Food Restaurant";
-    matn.style.cssText = 'color: white; font-size: 20px ';
+  body.style.cssText = 'background-size: 100%; background-image: url(http://images.summitmedia-digital.com/esquiremagph/images/2020/09/07/junk-food-aging.jpg); background-repeat: no-repeat';
+  homeDiv.textContent = 'Home Page';
+  homeDiv.style.cssText = 'color: white; text-align: center; font-family: sans-serif';
+  matn.textContent = "Welcome to Fast-Food Restaurant";
+  matn.style.cssText = 'color: white; font-size: 20px ';
 });
 
 // menu button
 menu.style.cssText = 'color: white; border: 1px solid white; text-align: center; font-size: 35px;';
 
 menu.addEventListener('click', () => {
-    body.style.cssText = 'background-size: 100%; background-image: url(https://hdwallsource.com/img/2019/4/fast-food-burgers-wallpaper-68908-71254-hd-wallpapers.jpg); background-repeat: no-repeat';
-    homeDiv.textContent = 'McDonald MENU:';
-    homeDiv.style.cssText = 'margin-left: 30px; color: white; width: 300px';
-    matn.textContent = ' Sandwich: 3$ Hamburger: 3.5$  Cheeseburger: 5$  French Fries: 1$  Cheetos Chicken : 4$   Hot-Dogs: 2$';
-    matn.style.cssText = 'border: 1px solid grey; border-radius: 15px; padding: 10px; color: white; font-size: 33px; width: 300px; opacity: 0.7; margin: 1px 0 0 30px;  box-shadow: 0 4px 8px 0 rgb(165, 164, 164), 0 6px 20px 0 rgb(165, 164, 164); ';
+  body.style.cssText = 'background-size: 100%; background-image: url(https://hdwallsource.com/img/2019/4/fast-food-burgers-wallpaper-68908-71254-hd-wallpapers.jpg); background-repeat: no-repeat';
+  homeDiv.textContent = 'McDonald MENU:';
+  homeDiv.style.cssText = 'margin-left: 30px; color: white; width: 300px';
+  matn.textContent = ' Sandwich: 3$ Hamburger: 3.5$  Cheeseburger: 5$  French Fries: 1$  Cheetos Chicken : 4$   Hot-Dogs: 2$';
+  matn.style.cssText = 'border: 1px solid grey; border-radius: 15px; padding: 10px; color: white; font-size: 33px; width: 300px; opacity: 0.7; margin: 1px 0 0 30px;  box-shadow: 0 4px 8px 0 rgb(165, 164, 164), 0 6px 20px 0 rgb(165, 164, 164); ';
 });
 
 // contact button
 contact.style.cssText = 'color: white; border: 1px solid white; text-align: center; font-size: 35px;';
 
 contact.addEventListener('click', () => {
-    body.style.cssText = 'background-size: 100%; background-image: url(https://www.isl.com.pk/wp-content/uploads/2020/10/header-img.png); background-repeat: no-repeat';
-    homeDiv.textContent = 'Contact Us';
-    homeDiv.style.cssText = 'color: white; text-align: center; font-family: sans-serif';
-    matn.textContent = 'If you want to contact us, please call the numbers below: +998907777777';
-    matn.style.cssText = 'color: white; font-size: 18px';
+  body.style.cssText = 'background-size: 100%; background-image: url(https://www.isl.com.pk/wp-content/uploads/2020/10/header-img.png); background-repeat: no-repeat';
+  homeDiv.textContent = 'Contact Us';
+  homeDiv.style.cssText = 'color: white; text-align: center; font-family: sans-serif';
+  matn.textContent = 'If you want to contact us, please call the numbers below: +998907777777';
+  matn.style.cssText = 'color: white; font-size: 18px';
 });
-
