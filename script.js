1098fc8b7ba4479d7128532d5493d42af283ac14
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:08:56 +0500

    header header.style.cssText = 'display: grid; grid-template-columns: 1fr 1fr 1fr;';

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const home = document.querySelector('.home');
 const menu = document.querySelector('.menu');
 const contact = document.querySelector('.contact');
 const body = document.querySelector('body');
+
+header.style.cssText = 'display: grid; grid-template-columns: 1fr 1fr 1fr;';
