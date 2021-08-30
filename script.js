2cc02d44f44bab1c3873315b902ff29b330fc5ad
Sanjik761 <sanjarburiniyozov@gmail.com>
2021-08-30 10:20:18 +0500

    created menu function

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ home.addEventListener('click', () => {
 
 // menu button
 menu.style.cssText = 'color: white; border: 1px solid white; text-align: center; font-size: 35px;';
+
+menu.addEventListener('click', () => {
+    body.style.cssText = 'background-size: 100%; background-image: url(https://hdwallsource.com/img/2019/4/fast-food-burgers-wallpaper-68908-71254-hd-wallpapers.jpg); background-repeat: no-repeat';
+    homeDiv.textContent = 'McDonald MENU:';
+    homeDiv.style.cssText = 'margin-left: 30px; color: white; width: 300px';
+    matn.textContent = ' Sandwich: 3$ Hamburger: 3.5$  Cheeseburger: 5$  French Fries: 1$  Cheetos Chicken : 4$   Hot-Dogs: 2$';
+    matn.style.cssText = 'border: 1px solid grey; border-radius: 15px; padding: 10px; color: white; font-size: 33px; width: 300px; opacity: 0.7; margin: 1px 0 0 30px;  box-shadow: 0 4px 8px 0 rgb(165, 164, 164), 0 6px 20px 0 rgb(165, 164, 164); ';
+});
